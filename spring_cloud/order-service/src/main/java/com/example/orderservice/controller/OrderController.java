7b79649e8f5f8ccc23269b4166344423c025c3e5
yt.kwon <ytkwon91@gmail.com>
2025-07-25 16:33:59 +0900

    till section 12.데이터 동기화를 위한 Apache Kafka의 활용 ②

--- spring_cloud/order-service/src/main/java/com/example/orderservice/controller/OrderController.java
+++ spring_cloud/order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -2,8 +2,8 @@ package com.example.orderservice.controller;
 
 import com.example.orderservice.dto.OrderDto;
 import com.example.orderservice.jpa.OrderEntity;
-//import com.example.orderservice.messagequeue.KafkaProducer;
-//import com.example.orderservice.messagequeue.OrderProducer;
+import com.example.orderservice.messagequeue.KafkaProducer;
+import com.example.orderservice.messagequeue.OrderProducer;
 import com.example.orderservice.service.OrderService;
 import com.example.orderservice.vo.RequestOrder;
 import com.example.orderservice.vo.ResponseOrder;
@@ -29,9 +29,8 @@ import java.util.UUID;
 public class OrderController {
     private final Environment env;
     private final OrderService orderService;
-//    private final KafkaProducer kafkaProducer;
-//
-//    private final OrderProducer orderProducer;
+    private final KafkaProducer kafkaProducer;
+    private final OrderProducer orderProducer;
 
     @GetMapping("/health_check")
     public String status() {
@@ -49,19 +48,20 @@ public class OrderController {
 
         OrderDto orderDto = mapper.map(orderDetails, OrderDto.class);
         orderDto.setUserId(userId);
-        /* jpa */
-        OrderDto createdOrder = orderService.createOrder(orderDto);
-        ResponseOrder responseOrder = mapper.map(createdOrder, ResponseOrder.class);
 
-        /* kafka */
+        /* jpa */
+//        OrderDto createdOrder = orderService.createOrder(orderDto);
+//        ResponseOrder responseOrder = mapper.map(createdOrder, ResponseOrder.class);
+
+//        /* kafka */
         orderDto.setOrderId(UUID.randomUUID().toString());
         orderDto.setTotalPrice(orderDetails.getQty() * orderDetails.getUnitPrice());
 
         /* send this order to the kafka */
-//        kafkaProducer.send("example-catalog-topic", orderDto);
-//        orderProducer.send("orders", orderDto);
+        kafkaProducer.send("example-catalog-topic", orderDto);
+        orderProducer.send("orders", orderDto);
 
-//        ResponseOrder responseOrder = mapper.map(orderDto, ResponseOrder.class);
+        ResponseOrder responseOrder = mapper.map(orderDto, ResponseOrder.class);
 
         log.info("After added orders data");
         return ResponseEntity.status(HttpStatus.CREATED).body(responseOrder);
